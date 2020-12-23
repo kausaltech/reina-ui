401a2a3bfca6c3d2020ef84868869b926a7d47da
Tero Tikkanen <tero.tikkanen@gmail.com>
2020-12-22 22:02:43 -0500

    Display active mobility limits on outcome

--- components/charts/MetricsGraph.js
+++ components/charts/MetricsGraph.js
@@ -5,9 +5,65 @@ import dayjs from 'dayjs';
 const DynamicPlot = dynamic(() => import('react-plotly.js'),
     { ssr: false });
 
+const createInterventionBar = (interventionSet, startDate, endDate, index) => {
+  const firstY = -0.2;
+  const barHeight = 0.05;
+  const barMargin = 0.06;
+  const barPosition = firstY - (barHeight + barMargin) * index;
+
+  // box outline for the bar
+  const bar =[{
+    type: 'rect',
+    yref: 'paper',
+    x0: startDate,
+    y0: barPosition,
+    x1: endDate,
+    y1: barPosition-barHeight,
+    line: {
+      color: '#e3e3e3',
+      width: 1,
+    }
+  }];
+
+  // category label for the bar
+  const label = {
+    xref: 'paper',
+    yref: 'paper',
+    x: 1.025,
+    y: barPosition - barHeight,
+    showarrow: false,
+    text: interventionSet.label,
+    xanchor: 'left',
+    yanchor: 'bottom',
+    yshift: -4,
+  };
+
+  // bar segments
+  interventionSet.interventions.forEach((intervention, idx) => {
+    const opacity = intervention.reduction/100;
+    let end = endDate;
+    if (interventionSet.interventions.length > idx + 1) end = interventionSet.interventions[idx+1].date;
+  
+    bar.push({
+      type: 'rect',
+      xref:'x',
+      yref:'paper',
+      x0: intervention.date,
+      y0: barPosition,
+      x1: end,
+      y1: barPosition-barHeight,
+      fillcolor: '#A73939',
+      opacity: opacity,
+      line: {
+        width: 0,
+      }
+    })
+  })
+  return { bar: bar, label: label };
+};
 
 function MetricsGraph(props) {
-  const { dailyMetrics, shownMetrics, title, validationMetrics } = props;
+  const { dailyMetrics, shownMetrics, title, validationMetrics, interventions } = props;
   const { metrics } = dailyMetrics;
 
   let metricsByType = new Map(metrics.map(m => [m.type, {metric: m, dates: dailyMetrics.dates}]));
@@ -43,28 +99,24 @@ function MetricsGraph(props) {
     };
   });
 
-  // Adds text annonation on current day line. Not in use for now
-  const annotations = [
-    {
-      x: new dayjs().format('YYYY-MM-DD'),
-      y: 1,
-      xref: 'x',
-      yref: 'paper',
-      text: 'Today',
-      showarrow: true,
-      arrowhead: 0,
-      arrowcolor: '#999999',
-      ax: 0,
-      ay: -15,
-    }
-  ];
+  let annotations = [];
+  let shapes = [];
 
-  const shapes = [
+  // create a horizontal bar graph for each intervention
+  const barCount = interventions ? interventions.length : 0;
+  if (barCount > 0) 
+    interventions.forEach((category, index) => {
+      const newBar = createInterventionBar(category, dailyMetrics.dates[0], dailyMetrics.dates[dailyMetrics.dates.length-1], index);
+      shapes = shapes.concat(newBar.bar);
+      annotations.push(newBar.label);
+    });
+
+  const todaymarker =
     {
       type: 'line',
       yref: 'paper',
       x0: new dayjs().format('YYYY-MM-DD'),
-      y0: 0,
+      y0: barCount * -0.3,
       x1: new dayjs().format('YYYY-MM-DD'),
       y1: 1,
       line: {
@@ -72,20 +124,28 @@ function MetricsGraph(props) {
         width: 2,
         dash: "dot",
       }
-    },
-  ];
+    };
+
+  shapes.push(todaymarker);
 
   const layout = {
-    title,
+    height: 300 + barCount * 25,
+    margin: {
+      t: 24,
+      r: 200,
+      b: 48 + barCount * 25,
+    },
     autosize: true,
     font: {
       family: 'Inter',
     },
-    shapes
+    shapes,
+    annotations,
   }
 
   return (
     <div>
+      <h4>{title}</h4>
       <DynamicPlot
           data={ traces }
           layout={ layout }
--- components/charts/PopulationGraph.js
+++ components/charts/PopulationGraph.js
@@ -1,8 +1,85 @@
 import React from 'react';
+import { gql, useQuery } from "@apollo/client";
 import MetricsGraph from './MetricsGraph';
+
+const GET_ACTIVE_INTERVENTIONS = gql`
+query GetActiveInvertions {
+  activeInterventions {
+    id
+    type
+    date
+    description
+    parameters {
+      id
+      description
+      ... on InterventionIntParameter {
+        value
+        unit
+      }
+      ... on InterventionChoiceParameter {
+        choice {
+          id
+          label
+        }
+      }
+    }
+  }
+}
+`;
+
+const categorizeMobilityInterventions = (interventions) => {
+  if(!interventions?.length) return null;
+
+  const mobilityInterventions = interventions.filter((element) => element.type==='LIMIT_MOBILITY');
+  const editedInterventions = [];
+  const interventionCategories= [];
+
+  // based on its parameters create a category label for each intervention
+  mobilityInterventions.forEach((element) => {
+      const minAge = element.parameters.find((param) => param.id==='min_age');
+      const maxAge = element.parameters.find((param) => param.id==='max_age');
+      const place = element.parameters.find((param) => param.id==='place');
+      const reduction = element.parameters.find((param) => param.id==='reduction');
+
+      const ageGroup = (minAge || maxAge) ?
+        ` (${minAge ? minAge.value : 0}-${maxAge ? maxAge.value : 100})` : undefined;
+      const categoryLabel = `${place ? place.choice.label : 'All'}${ageGroup || ''}`;
+
+      editedInterventions.push({
+        label: categoryLabel,
+        reduction: reduction.value,
+        date: element.date,
+        id: element.id,
+      });
+
+      interventionCategories.push(categoryLabel);
+  });
+
+  // create a list of unique intervention categories by label
+  const uniqueCategories = Array.from(new Set(interventionCategories)).sort();
+  const categorizedInterventions = [];
+  uniqueCategories.forEach((cat) => {
+    const category = editedInterventions.filter((intervention) => intervention.label === cat);
+    categorizedInterventions.push({
+      interventions: category,
+      label: cat,
+    })
+  })
+  return categorizedInterventions;
+};
 
 function PopulationGraph(props) {
   const { dailyMetrics } = props;
+
+  const { loading: loadingActive, error: errorActive, data: dataActive } = useQuery(GET_ACTIVE_INTERVENTIONS);
+
+  if (loadingActive) {
+    return <div>Loading interventions</div>
+  }
+  if (errorActive) {
+    console.log(error);
+    return <div>Err</div>
+  }
 
   const shownMetrics = [
     { type: 'SUSCEPTIBLE', visible: 'legendonly' },
@@ -12,7 +89,12 @@ function PopulationGraph(props) {
     { type: 'DEAD' },
     { type: 'RECOVERED', visible: 'legendonly' },
   ]
-  return <MetricsGraph dailyMetrics={dailyMetrics} shownMetrics={shownMetrics} title="Population" />
+  return <MetricsGraph
+    dailyMetrics={dailyMetrics}
+    shownMetrics={shownMetrics}
+    interventions={categorizeMobilityInterventions(dataActive.activeInterventions)}
+    title="Population"
+  />
 }
 
 export default PopulationGraph;
